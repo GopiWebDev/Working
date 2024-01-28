1d0a0eeba120b863cc4bc8939e803892d5bade59
GopiWebDev <gopisv.webdev@gmail.com>
2024-01-28 14:14:05 +0530

    added delBtn function

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const divide = (a, b) => a / b;
 let operatorValue = "";
 let previousValue = "";
 let currentValue = "";
-
+let result = "";
 // Step 3 - Create a new function operate that takes an operator and 2 numbers
 
 function operate(operator, num1, num2) {
@@ -94,27 +94,36 @@ operators.forEach((op) => {
 
 // Step 6 - Make the calculator work!
 
-equal.addEventListener("click", () => {
+equal.addEventListener("click", equals);
+
+function equals() {
   if (currentValue !== "" && previousValue !== "" && operatorValue !== "") {
     previousValue = Number(previousValue);
     currentValue = Number(currentValue);
     result = operate(operatorValue, previousValue, currentValue);
-    console.log(result);
     previousOperand.textContent = "";
     currentOperand.textContent = result;
   }
-});
+}
 
 // Step 7
 
 // clear button resets everything
-clearBtn.addEventListener("click", () => {
+clearBtn.addEventListener("click", clearAll);
+
+function clearAll() {
   previousValue = "";
   currentValue = "";
   operatorValue = "";
   previousOperand.textContent = "";
   currentOperand.textContent = "";
-});
+  result = "";
+}
 
 // Delete button
-deleteBtn.addEventListener("click", () => {});
+deleteBtn.addEventListener("click", deleteNumber);
+
+function deleteNumber() {
+  currentValue = currentValue.toString().slice(0, -1);
+  currentOperand.textContent = currentValue;
+}
